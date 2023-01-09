8362390463e3ba3aa009cea20d028854f56be763
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-09 13:29:29 -0600

    Add item class and methods

---
+++ item.rb
@@ -0,0 +1,19 @@
+class Item
+
+  def initialize(book, music_album, game)
+    @book = book
+    @music_album = music_album
+    @game = game
+  end
+
+  def can_be_archived?(published_date)
+    published_date > 10
+  end
+
+  def move_to_archive(item)
+
+    if can_be_archived?
+      item.archived = false  
+    end
+  end
+end
