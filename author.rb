a63642405e1b4434acc57e1736306c672bc51f4f
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-09 16:10:15 -0600

    Add author class

---
+++ author.rb
@@ -0,0 +1,12 @@
+class Author
+  private
+  attr_accessor id, items
+  
+  def initialize(id, first_name, last_name, items)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = items
+  end
+
+end
