de2afe4190ab3a4acbb03cfd5cf4f8edc437d982
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-10 10:31:32 -0600

    Improve code for convention with coding-partners

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 require_relative 'item'
-class Game
+class Game < Item
   def initialize(multiplayer, last_played_at)
+    super(game)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
