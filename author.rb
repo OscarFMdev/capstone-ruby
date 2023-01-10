e2977efc1ab6f0365a3163a05ed0719b86738aaa
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-10 10:40:01 -0600

    Ad random ID to author

--- author.rb
+++ author.rb
@@ -2,7 +2,7 @@ class Author
   attr_accessor :first_name, :last_name
 
   def initialize(id, first_name, last_name, items)
-    @id = id
+    @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = items
