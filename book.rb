6de66fac23ce2044961ea5109f4aa617fb94c2f9
IngDavidTM <david-1011@hotmail.com>
2023-01-10 10:10:38 -0500

    Add book properties and methods

--- book.rb
+++ book.rb
@@ -1,2 +1,12 @@
 class Book
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?(published_date)
+    published_date > 10
+  end
 end
