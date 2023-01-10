a1eef0c04bd50033c69dd8e7777ed3001c31d8d8
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-10 11:37:57 -0600

    Update author structure

--- author.rb
+++ author.rb
@@ -1,14 +1,22 @@
 class Author
   attr_accessor :first_name, :last_name
 
-  def initialize(id, first_name, last_name, items)
+  def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
-    @items = items
+    @items = []
   end
 
-  private
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
 
+  private 
   attr_accessor :id, :items
 end
+
+
+author = Author.new('Hello', 'World')
+p author.items
