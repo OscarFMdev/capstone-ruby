ad553862a8748717485178d5529e851725f89095
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-10 10:32:02 -0600

    Fix linter errors

--- author.rb
+++ author.rb
@@ -1,12 +1,13 @@
 class Author
   attr_accessor :first_name, :last_name
-  
+
   def initialize(id, first_name, last_name, items)
     @id = id
     @first_name = first_name
     @last_name = last_name
     @items = items
   end
+
   private
 
   attr_accessor :id, :items
