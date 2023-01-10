d3aa824ed9fafbfd682613ee3de37c7084067108
IngDavidTM <david-1011@hotmail.com>
2023-01-10 15:37:07 -0500

    Remove label from item test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,7 @@
 require_relative './../item'
-require_relative './../label'
 
 describe 'Test Item class' do
   item = Item.new(published_date: '25/10/2023')
-  label = Label.new('test1', 'black')
 
   it 'takes parameters and returns a item object' do
     expect(item).to be_instance_of(Item)
