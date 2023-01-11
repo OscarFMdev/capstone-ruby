c95c6803d0a61e0355f9e109578be6bd408b02b3
IngDavidTM <david-1011@hotmail.com>
2023-01-11 09:58:47 -0500

    Fix Item class

--- item.rb
+++ item.rb
@@ -9,19 +9,12 @@ class Item
     @published_date = published_date
   end
 
-  def can_be_archived?
-    @published_date > 10
-  end
-
   def move_to_archive
     @archive = can_be_archived?
     can_be_archived?
   end
 
-  def label=(label)
-    @label = label
-    label.items.push(self) unless label.items.include?(self)
-  end
+  attr_writer :label
 
   def author=(author)
     @author = author
@@ -31,5 +24,13 @@ class Item
   def genre=(genre)
     @genre = genre
     genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  private
+
+  attr_accessor :id, :archive
+
+  def can_be_archived?
+    @published_date > 10
   end
 end
