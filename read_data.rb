fe6a065978c0c963a72ed9f48664e2050e4641dd
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-01-11 11:15:23 -0600

    Handle reading json files 📖

--- read_data.rb
+++ read_data.rb
@@ -14,4 +14,19 @@ module ReadData
     json_from_file = File.read('./data/labels.json')
     JSON.parse(json_from_file)
   end
+
+  def read_games
+    return [] unless File.exist?('./data/games.json') && !File.zero?('./data/games.json')
+
+    json_from_file = File.read('./data/games.json')
+    JSON.parse(json_from_file)
+  end
+
+  def read_authors
+    return [] unless File.exist?('./data/authors.json') && !File.zero?('./data/authors.json')
+
+    json_from_file = File.read('./data/authors.json')
+    JSON.parse(json_from_file)
+  end
+
 end
