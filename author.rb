0d891cc9516907d63e56e9889e8d3f734d632433
IngDavidTM <david-1011@hotmail.com>
2023-01-12 10:41:31 -0500

    Fix private methods

--- author.rb
+++ author.rb
@@ -1,5 +1,5 @@
 class Author
-  attr_accessor :first_name, :last_name, :id, :items
+  attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @id = Random.rand(1..1000)
@@ -13,7 +13,7 @@ class Author
     item.author = self
   end
 
-  # private
+  private
 
-  # attr_accessor :id, :items
+  attr_accessor :id, :items
 end
